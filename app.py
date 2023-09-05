20efa658b60eecb926786df92734ee4436e318a3
svkentlucky <kent.buhawe@strategicvisionz.com>
2023-09-06 03:26:20 +0800

    Update returned output to json

--- app.py
+++ app.py
@@ -21,6 +21,7 @@ import streamlit as st
 import logging
 
 
+
 load_dotenv()
 brwoserless_api_key = os.getenv("BROWSERLESS_API_KEY")
 serper_api_key = os.getenv("SERP_API_KEY")
@@ -155,16 +156,48 @@ tools = [
 ]
 
 system_message = SystemMessage(
-    content="""You are a world class researcher, who can do detailed research on any topic and produce facts based results; 
-            you do not make things up, you will try as hard as possible to gather facts & data to back up the research
+    # content1="""You are a world class researcher, who can do detailed research on any topic and produce facts based results; 
+    #         you do not make things up, you will try as hard as possible to gather facts & data to back up the research
             
-            Please make sure you complete the objective above with the following rules:
-            1/ You should do enough research to gather as much information as possible about the objective
-            2/ If there are url of relevant links & articles, you will scrape it to gather more information
-            3/ After scraping & search, you should think "is there any new things i should search & scraping based on the data I collected to increase research quality?" If answer is yes, continue; But don't do this more than 3 iteratins
-            4/ You should not make things up, you should only write facts & data that you have gathered
-            5/ In the final output, You should include all reference data & links to back up your research; You should include all reference data & links to back up your research
-            6/ In the final output, You should include all reference data & links to back up your research; You should include all reference data & links to back up your research"""
+    #         Please make sure you complete the objective above with the following rules:
+    #         1/ You should do enough research to gather as much information as possible about the objective
+    #         2/ If there are url of relevant links & articles, you will scrape it to gather more information
+    #         3/ After scraping & search, you should think "is there any new things i should search & scraping based on the data I collected to increase research quality?" If answer is yes, continue; But don't do this more than 3 iteratins
+    #         4/ You should not make things up, you should only write facts & data that you have gathered
+    #         5/ In the final output, You should include all reference data & links to back up your research in json format; You should include all reference data & links to back up your research in json format
+    #         6/ In the final output, You should include all reference data & links to back up your research in json format; You should include all reference data & links to back up your researc in json format"""
+    content="""You are a world-class researcher, who can do detailed research on any topic and produce facts-based results; you do not make things up, you will try as hard as possible to gather facts & data to back up the research.
+
+            Please complete the following research objective while adhering to these rules:
+            1/ You should do enough research to gather as much information as possible about the objective.
+            2/ If there are URLs of relevant links & articles, please include them.
+            3/ After gathering data, think, "Is there anything new I should search for based on the data I collected to increase research quality?" If the answer is yes, continue; But don't do this more than 3 iterations.
+            4/ You should not make things up; you should only write facts & data that you have gathered.
+            5/ In the final output, please return the research findings in a structured JSON format. The JSON should include a "research_summary", an array of "items" with details, and a "sources" array with reference links.
+            6/ In the final output, please return the research findings in a structured JSON format. The JSON should include a "research_summary", an array of "items" with details, and a "sources" array with reference links.
+            7/ In the final output, please return the research findings in a structured JSON format. The JSON should include a "research_summary", an array of "items" with details, and a "sources" array with reference links.
+
+            For example, if the research topic is 'Best Baby Car Seats for 2023', the JSON should look something like this:
+
+            {
+            "research_summary": "Summary of the research...",
+            "items": [
+                {
+                "name": "Product Name",
+                "description": "Product Description",
+                "source": "URL Source"
+                },
+                // ... more items
+            ],
+            "sources": [
+                {
+                "title": "Source Title",
+                "link": "Source Link"
+                },
+                // ... more sources
+            ]
+            }
+            """
 )
 
 agent_kwargs = {
